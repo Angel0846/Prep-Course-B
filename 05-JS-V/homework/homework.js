0c35b20c2ed2f2b2e6d7a79d000b627a9373c245
angel846 <angel_martinez846@hotmail.com>
2022-11-04 16:12:07 -0600

    tareas terminadas

--- 05-JS-V/homework/homework.js
+++ 05-JS-V/homework/homework.js
@@ -4,16 +4,37 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` 
+  //que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-}
+  class Usuario{
+    constructor(opciones) {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+      }
+    }
+  Usuario.prototype.saludar = function() {
+    return "Hola, mi nombre es " + this.nombre;
+  }
+
+    return Usuario;
+  }
+
+   
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar= function(){
+    return "Hello World!";
+  }
+
 }
 
 function agregarStringInvertida() {
@@ -21,13 +42,23 @@ function agregarStringInvertida() {
   // El método debe llamarse "reverse"
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
-  // Pista: Necesitarás usar "this" dentro de "reverse"
+  // Pista: Necesitarás ;sar "this" dentro de "reverse"
+  String.prototype.reverse = function (string){
+    var longitud = this.length;
+    var strReverse ="";
+
+    for(var i=longitud-1;i>=0;i--){
+      strReverse+=this[i];
+  }
+  return strReverse;
 }
 
+}
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
-  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona 
+  //y sus valores.
   //Ej: { 
     //   Nombre: 'Juan',
     //   Apellido: 'Perez',
@@ -36,8 +67,19 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor( nombre, apellido, edad, domicilio) {
       // Crea el constructor:
+      this.nombre= nombre;
+      this.apellido= apellido;
+      this.edad= edad;
+      this.domicilio= domicilio;
+      this.detalle = function(){
+        return {
+          Nombre: this.nombre,
+          Apellido: this.apellido,
+          Edad: this.edad,
+          Domicilio: this.domicilio};
+      }
 
     }
 }
@@ -46,11 +88,25 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var persona = new Persona;
+  persona.nombre= nombre;
+  persona.apellido = apellido;
+  persona.edad = edad;
+  persona.dir = dir;
+
+    //opcion 2, var persona = new Persona (nombre, apellido,edad, dir);
+
+  return persona;
+
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos= function(){
+    return this.nombre+", "+ this.edad + " años";
+  }
+
 }
   
 
@@ -64,4 +120,4 @@ module.exports = {
   crearInstanciaPersona,
   agregarMetodo, 
   Persona
-};
+}
